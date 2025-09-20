a217bc99f53561755b5fbd2934098fac839c3ed9
RameshDeveloper001 <chramesh7526@gmail.com>
2025-09-20 13:45:48 +0530

    GetSalarySalaryDetails

---
+++ Mobile_Core/ViewModel/SalarysDetailsParameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile_Core.ViewModel
+{
+    public class SalarysDetailsParameter
+    {
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public int EmployeeId { get; set; }
+
+    }
+}
--- Mobile_Infrastructure/Repository/EmpployeeManage/EmpployeeManageRepository.cs
+++ Mobile_Infrastructure/Repository/EmpployeeManage/EmpployeeManageRepository.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using Mobile_Core.CommonClass;
 using Mobile_Core.DB;
+using Mobile_Core.ViewModel;
 using Mobile_Core.ViewModel.Employee;
 using Mobile_Infrastructure.Interface.EmployeeAttedance;
 using Mobile_Infrastructure.Interface.EmpployeeManage;
@@ -54,6 +55,19 @@ namespace Mobile_Infrastructure.Repository.EmpployeeManage
             catch (Exception)
             {
                 return new SP_Response { Success = false, Message = "Something went wrong!" };
+            }
+        }
+        public async Task<List<vmGetSalarySalaryDetails>> GetSalarySalaryDetails(SalarysDetailsParameter vm)
+        {
+            try
+            {
+
+                var result = await _db.Set<vmGetSalarySalaryDetails>().FromSqlInterpolated($"EXEC SP_Mobile_GetSalarySalaryDetails @EmployeeId={vm.EmployeeId},@Month={vm.Month},@Year={vm.Year}").ToListAsync();
+                return result;
+            }
+            catch
+            {
+                return new List<vmGetSalarySalaryDetails>();
             }
         }
     }
