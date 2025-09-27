d7ab1224a16122ce3f0e6358f0297fd3b62b3f92
RameshDeveloper001 <chramesh7526@gmail.com>
2025-09-27 13:14:57 +0530

    Leave Approval

---
+++ Mobile_Core/EmployeeAttedance/LeaveApplication.cs
@@ -0,0 +1,15 @@
+﻿namespace Mobile_Core.EmployeeAttedance
+{
+    public class LeaveApplication
+    {
+        public int? EmployeeId { get; set; }
+        public int? LeaveTypeId { get; set; }
+        public DateTime? FromDate { get; set; }
+        public decimal? No_Of_Date { get; set; }
+        public DateTime? Todate { get; set; }
+        public string? Reason { get; set; }
+        public int? Responsibleperson { get; set; }
+        public string? HalfDayType { get; set; }
+        public string? CreatedBy { get; set; }
+    }
+}
--- Mobile_Infrastructure/Repository/EmployeeAttedance/EmployeeLeaveManageRepository.cs
+++ Mobile_Infrastructure/Repository/EmployeeAttedance/EmployeeLeaveManageRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Mobile_Core.CommonClass;
 using Mobile_Core.DB;
+using Mobile_Core.EmployeeAttedance;
 using Mobile_Core.ViewModel.EmployeeReport;
 using Mobile_Infrastructure.Interface.EmployeeAttedance;
 using Mobile_Utility;
@@ -16,6 +17,41 @@ namespace Mobile_Infrastructure.Repository.EmployeeAttedance
             _db = db;
         }
 
+        public async Task<APIResponse> CreateLeaveApplication(LeaveApplication model)
+        {
+            try
+            {
+                var result = await _db.Set<SP_Response>().FromSqlInterpolated($@"
+                EXEC SP_Mobile_LeaveApplication
+                    @Action={"INSERT"}, 
+                    @EmployeeId = {model.EmployeeId},
+                    @LeaveTypeId = {model.LeaveTypeId},
+                     @FromDate= {model.FromDate},
+                    @No_Of_Date = {model.No_Of_Date},
+                     @Todate= {model.Todate},
+                     @Reason= {model.Reason},
+                     @Responsibleperson= {model.Responsibleperson},
+                     @HalfDayType= {model.HalfDayType},
+                     @CreatedBy= {model.CreatedBy}
+
+            ").ToListAsync();
+
+                var data = result?.FirstOrDefault() ?? null;
+                if (data != null)
+                {
+                    return new APIResponse { Status = data.Success, ResponseMessage = data.Message };
+                }
+                else
+                {
+                    return new APIResponse { Status = false, ResponseMessage = "Something went wrong" };
+                }
+
+            }
+            catch
+            {
+                return new APIResponse { Status = false, ResponseMessage = "Something went wrong" };
+            }
+        }
         public async Task<APIResponse> LeaveApproveOrReject(ApprovalModel model)
         {
             try
